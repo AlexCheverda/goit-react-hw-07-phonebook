223d413c78a30314206faea11664f7076b91fc86
Aleksandr Cheverda <alexar@ua.fm>
2022-12-26 19:21:31 +0200

    Update reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,13 +1,15 @@
 import { createReducer } from "@reduxjs/toolkit";
 
-import { addContact, deleteContact, filterContact } from './actions';
-import { INITIAL_CONTACTS } from 'data/data';
+import { filterContact } from "./actions";
 
-export const contacts = createReducer(INITIAL_CONTACTS, {
-    [addContact.type]: (state, action) => [...state, action.payload],
-    [deleteContact.type]: (state, action) =>
-        state.filter(({ id }) => id !== action.payload),
-});
+// import { addContact, deleteContact, filterContact } from './actions';
+// import { INITIAL_CONTACTS } from 'data/data';
+
+// export const contacts = createReducer(INITIAL_CONTACTS, {
+//     [addContact.type]: (state, action) => [...state, action.payload],
+//     [deleteContact.type]: (state, action) =>
+//         state.filter(({ id }) => id !== action.payload),
+// });
 
 export const filter = createReducer('', {
     [filterContact.type]: (_, action) => action.payload.toLowerCase(),
