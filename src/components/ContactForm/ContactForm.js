4d1ccde456b0f60e086858ad15006e779775d555
Aleksandr Cheverda <alexar@ua.fm>
2022-12-26 19:54:25 +0200

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,50 +1,71 @@
+import Notiflix from "notiflix";
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { addContact } from "redux/actions";
+import {
+  useGetContactsQuery,
+  useAddContactMutation,
+} from '../../redux/contactSlice';
+// import { useDispatch, useSelector } from "react-redux";
+// import { addContact } from "redux/actions";
 import { nanoid } from "nanoid";
 // import PropTypes from 'prop-types';
 import { Form, Input, Label, FormBtn } from './ContactForm.Styled';
 
 const ContactForm = () => {
-  const contacts = useSelector(state => state.contacts);
-  const [userName, setUserName] = useState('');
+  const { data } = useGetContactsQuery();
+  const [addContact] = useAddContactMutation();
+  const [name, setName] = useState('');
+  // const contacts = useSelector(state => state.contacts);
+  // const [userName, setUserName] = useState('');
   const [number, setNumber] = useState('');
 
   const nameInputId = nanoid();
   const numberInputId = nanoid();
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   const handleInputChange = evt => {
     const { name, value } = evt.currentTarget;
     if (name === 'name') {
-      setUserName(value);
+      setName(value);
     }
     if (name === 'number') {
       setNumber(value);
     }
   };
 
-  const handleSubmitForm = evt => {
+  // const handleSubmitForm = evt => {
+  //   evt.preventDefault();
+  //   contacts.find(({ name }) => name === userName)
+  //     ? alert(`${userName} is already in contacts.`)
+  //     : dispatch(addContact(userName, number));
+  //   reset();
+  // };
+
+  const handleAddContact = async evt => {
     evt.preventDefault();
-    contacts.find(({ name }) => name === userName)
-      ? alert(`${userName} is already in contacts.`)
-      : dispatch(addContact(userName, number));
+    try {
+      data.find(contact => contact.name === name)
+        ? Notiflix.Notify.info(`${name} is already in contacts.`)
+        : (await addContact({ name, number })) &&
+        Notiflix.Notify.success(`${name} added to your phonebook`);
+    } catch (error) {
+      console.log(error);
+    }
     reset();
-  };
+  };  
 
   const reset = () => {
-    setUserName('');
+    setName('');
     setNumber('');
   };
 
   return (
-    <Form onSubmit={handleSubmitForm}>
+    <Form onSubmit={handleAddContact}>
       <Label htmlFor={nameInputId}>Name</Label>
       <Input
         type="text"
         name="name"
-        value={userName}
+        value={name}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
