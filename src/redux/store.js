1e58d3ad535031903c0d07f7ccdabac2d5567673
Aleksandr Cheverda <alexar@ua.fm>
2022-12-26 21:50:31 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { contactsApi } from "./contactSlice";
 import { filter } from './reducers';
 
